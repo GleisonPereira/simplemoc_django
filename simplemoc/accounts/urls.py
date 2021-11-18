2ff833ecc248c9aa7ec450923623922d567be8bf
GleisonPereira <gleisongpm@gmail.com>
2021-11-18 16:47:33 -0300

    aula 38

--- simplemoc/accounts/urls.py
+++ simplemoc/accounts/urls.py
@@ -3,5 +3,6 @@ from django.contrib.auth import views as auth_views
 from simplemoc.accounts.views import register
 urlpatterns = [
     path('entrar/',auth_views.LoginView.as_view(),name='login'),
+    path('sair/',auth_views.LogoutView.as_view(),name='logout'),
     path('cadastre-se/', register, name='register'),
 ]
